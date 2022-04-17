d1daf6d4db8b9bcd314981296f38182fb90055b7
Brent Vreys <69217508+Brent1369@users.noreply.github.com>
2022-04-17 19:41:43 +0200

    hdmi test werkt

---
+++ test/testhdmi/src/helloworld.c
@@ -0,0 +1,53 @@
+#include <stdio.h>
+#include "platform.h"
+#include "xil_printf.h"
+#include "xv_tpg.h"
+#include "sleep.h"
+#include "xvtc.h"
+
+XV_tpg tpg_inst;
+int Status;
+
+int main()
+{
+    init_platform();
+
+    print("Hello World\n\r");
+
+    /* TPG Initialization */
+    Status = XV_tpg_Initialize(&tpg_inst, XPAR_V_TPG_0_DEVICE_ID);
+    if(Status!= XST_SUCCESS)
+    {
+    	xil_printf("TPG configuration failed\r\n");
+        return(XST_FAILURE);
+    }
+
+    // Set Resolution to 800x600
+    XV_tpg_Set_height(&tpg_inst, 1080);
+    XV_tpg_Set_width(&tpg_inst, 1920);
+
+    // Set Color Space to RGB
+    XV_tpg_Set_colorFormat(&tpg_inst, 0x0);
+
+    //Set pattern to color bar
+    XV_tpg_Set_bckgndId(&tpg_inst, XTPG_BKGND_COLOR_BARS);
+
+    //Start the TPG
+    XV_tpg_EnableAutoRestart(&tpg_inst);
+    XV_tpg_Start(&tpg_inst);
+
+    XVtc_Enable(XPAR_V_TC_0_DEVICE_ID);
+
+    xil_printf("TPG started!\r\n");
+
+
+    /* End of TPG code*/
+    usleep(10000000);
+    for(;;){
+    	XV_tpg_Set_bckgndId(&tpg_inst, XTPG_BKGND_RAINBOW_COLOR);
+    	usleep(1000000);
+    }
+
+    cleanup_platform();
+    return 0;
+}
